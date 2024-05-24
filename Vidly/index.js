3d5199d02da6d32c3fea47df27ee1ab05caca83c
Kartheek Midde <kartheek.midde@clearlink.com>
2024-05-23 21:56:30 -0600

    Updated app structire for Vidly and Express app

--- Vidly/index.js
+++ Vidly/index.js
@@ -1,76 +1,14 @@
 const express = require('express')
 const app = express()
-const Joi = require('joi')
+const genres = require('./routes/genres')
 
 app.use(express.json())
+app.use('/api/genres', genres)
 
 const port = process.env.PORT | 3000
-const genres = [
-    { id: 1, name: "action"}, 
-    { id: 2, name: "thriller"}, 
-    { id: 3, name: "drama"}
-]
 
 app.get('/', (req, res) => {
     res.send('OK')
 })
-
-app.get('/api/genres', (req, res) => {
-    res.send(genres)
-})
-
-app.get('/api/genres/:id', (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id))
-    if (!genre)
-        return res.status(404).send('The genre with given id is not found')
-    res.send(genre)
-})
-
-app.post('/api/genres', (req, res) => {
-    const { error } = validateGenre(req.body)
-    if (error)
-        return res.status(400).send(error.details[0].message)
-
-    const genre = {
-        id: genres.length + 1,
-        name: req.body.name
-    }
-
-    genres.push(genre)
-    return res.send(genre)
-})
-
-app.put('/api/genres/:id', (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id))
-    if (!genre)
-        return res.status(404).send('The genre with given id is not found')
-
-    const { error } = validateGenre(req.body)
-    if (error)
-        return res.status(400).send(error.details[0].message)
-
-    genre.name = req.body.name
-    res.send(genre)
-
-})
-
-app.delete('/api/genres/:id', (req, res) => {
-    const genre = genres.find(gen => gen.id === parseInt(req.params.id))
-    if (!genre)
-        return res.status(404).send('The genre with given id is not found')
-
-    const index = genres.indexOf(genre)
-    genres.splice(index, 1)
-
-    return res.send(genre)
-})
-
-function validateGenre(genre) {
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    })
-
-    return schema.validate(genre)
-}
 
 app.listen(port, () => console.log(`Listening to port ${port}...`))
